8230351245501d14e973fc181066f3c4fc39be3b
paolobeta71 <47487300+paolobeta71@users.noreply.github.com>
2022-12-07 17:29:28 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,18 +19,26 @@ resource "azurerm_resource_group" "nginx-scaleset-pp" {
 }
 
 resource "azurerm_virtual_network" "nginx-scaleset-pp" {
- name                = "nginx-scaleset-pp-vnet"
- address_space       = ["10.0.0.0/16"]
+ name                = var.scaleset_vnet_name
+ address_space       = [var.scaleset_address_space]
  location            = var.location
  resource_group_name = azurerm_resource_group.nginx-scaleset-pp.name
 }
 
 resource "azurerm_subnet" "nginx-scaleset-pp" {
- name                 = "nginx-scaleset-pp-subnet"
+ name                 = var.scaleset_subnet_name
  resource_group_name  = azurerm_resource_group.nginx-scaleset-pp.name
  virtual_network_name = azurerm_virtual_network.nginx-scaleset-pp.name
- address_prefixes       = [var.subnet]
+ address_prefixes       = [var.scaleset_subnet]
 }
+
+resource "azurerm_public_ip" "nginx-scaleset-pp" {
+ name                         = "nginx-scaleset-pp-public-ip"
+ location                     = var.location
+ resource_group_name          = azurerm_resource_group.nginx-scaleset-pp.name
+ allocation_method            = "Static"
+}
+
 
 resource "azurerm_lb" "nginx-scaleset-pp" {
  name                = "nginx-scaleset-pp-lb"
@@ -38,11 +46,9 @@ resource "azurerm_lb" "nginx-scaleset-pp" {
  resource_group_name = azurerm_resource_group.nginx-scaleset-pp.name
 
  frontend_ip_configuration {
-   name                 = "PrivateIPAddress"
-   subnet_id         = azurerm_subnet.nginx-scaleset-pp.id
-   private_ip_address_allocation = "Static"
-   private_ip_address   = var.LB_StaticIP
-   private_ip_address_version = "IPv4"
+   name                 = "LB-PublicIPAddress"
+ #  subnet_id         = azurerm_subnet.nginx-scaleset-pp.id
+   public_ip_address_id = azurerm_public_ip.nginx-scaleset-pp.id
  }
 
 }
@@ -127,6 +133,34 @@ resource "azurerm_lb_rule" "lbnatrule" {
    frontend_port                  = var.fe_application_port
    backend_port                   = var.be_application_port
    backend_address_pool_ids        = [azurerm_lb_backend_address_pool.backend-addr-pool.id]
-   frontend_ip_configuration_name = "PrivateIPAddress"
+   frontend_ip_configuration_name = "LB-PublicIPAddress"
    probe_id                       = azurerm_lb_probe.nginx-scaleset-pp.id
+}
+
+resource "azurerm_virtual_network_peering" "vnet_peering_name" {
+  name                      = var.vnet_peering_name
+  resource_group_name       = azurerm_resource_group.nginx-scaleset-pp.name
+  virtual_network_name      = azurerm_virtual_network.nginx-scaleset-pp.name
+  remote_virtual_network_id = var.remote_virtual_network_id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic   = true
+}
+
+data "azurerm_resource_group" "remote-rg" {
+  name = var.remote_rg_name
+}
+
+data "azurerm_virtual_network" "remote-vnet" {
+  resource_group_name       = data.azurerm_resource_group.remote-rg.name
+  name                      = var.remote_vnet_name
+}
+
+resource "azurerm_virtual_network_peering" "remote_vnet_peering_name" {
+  name                      = var.remote_vnet_peering_name
+  resource_group_name       = data.azurerm_resource_group.remote-rg.name
+  remote_virtual_network_id = var.local_virtual_network_id
+  virtual_network_name      = data.azurerm_virtual_network.remote-vnet.name
+  allow_virtual_network_access = true
+  allow_forwarded_traffic   = true
+  depends_on = [azurerm_virtual_network_peering.vnet_peering_name]
 }
